6dc106d2d7c6844114b3c8e21f85a363c58f137c
yeshuwei <ysw@webull.com>
2018-07-25 22:00:55 +0800

    add mvp-model

---
+++ app/src/main/java/com/github/wan/BaseModel.kt
@@ -0,0 +1,6 @@
+package com.github.wan
+
+/**
+ * Create by swyww on 2018/7/25
+ */
+open class BaseModel {}
--- app/src/main/java/com/github/wan/home/HomePresenter.kt
+++ app/src/main/java/com/github/wan/home/HomePresenter.kt
@@ -2,6 +2,7 @@ package com.github.wan.home
 
 import android.content.Context
 import com.github.wan.bean.ArticleItem
+import com.github.wan.bean.ArticleItemBean
 import com.github.wan.bean.BannerBean
 import com.github.wan.extentions.networkConnected
 import com.github.wan.net.RetrofitClient
@@ -9,15 +10,17 @@ import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 import java.util.ArrayList
 
-class HomePresenter(context: Context, pageView: HomeContract.View) : HomeContract.Presenter {
+class HomePresenter(context: Context, pageView: HomeContract.View) : HomeContract.Presenter, HomeModel.IHomeModelView {
 
     private val mContext: Context = context
     private var mPage: Int = 0
     private val mPageView: HomeContract.View = pageView
     private val bannerUrl: MutableList<String> = ArrayList()
+    private val model: HomeModel
 
     init {
         mPageView.presenter = this
+        model = HomeModel(this)
     }
 
     override fun start() {
@@ -35,40 +38,22 @@ class HomePresenter(context: Context, pageView: HomeContract.View) : HomeContrac
 
     private fun load(page: Int) {
         if (networkConnected(mContext)) {
-            RetrofitClient.getWanApi()!!.getArticleList(page)
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe { it: ArticleItem? ->
-                        if (it != null) {
-                            mPageView.setData(it.data.datas)
-                        }
-                    }
+            model.getArticleList(page)
         }
     }
 
     override fun getBannerData() {
         if (networkConnected(mContext)) {
-            RetrofitClient.getWanApi()!!.getBanner()
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe { it: BannerBean? ->
-                        if (it?.data != null) {
-                            filterImageAndTitle(it)
-                        }
-                    }
+            model.getBanner()
         }
     }
 
-    private fun filterImageAndTitle(bannerBean: BannerBean) {
-        val images: MutableList<Any> = ArrayList()
-        val titles: MutableList<String> = ArrayList()
-        val i = 0
-        for (item in bannerBean.data!!) {
-            images.add(i, item.imagePath)
-            titles.add(i, item.title)
-            bannerUrl.add(i, item.url)
-            i.inc()
-        }
+    override fun setArticleList(articles: List<ArticleItemBean>) {
+        mPageView.setData(articles)
+    }
+
+    override fun setBanner(images: List<Any>, titles: List<String>, bannerUrls: List<String>) {
+        bannerUrl.addAll(bannerUrls)
         mPageView.setBannerData(images, titles)
     }
 
